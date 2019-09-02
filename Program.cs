acbe27b47e2dfce67ac7ccb417b0bfa467fa4b81
Gogs <gogs@fake.local>
2019-09-02 22:03:41 +0430

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trello.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
